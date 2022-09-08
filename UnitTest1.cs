50dd476e7b3bc3d3cb080564b685878788e49e95
blacOOil <111561598+blacOOil@users.noreply.github.com>
2022-09-08 18:57:05 +0700

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace tester;
+
+public class UnitTest1
+{
+     [Fact]
+    public void inputInformation() {
+        
+
+    }
+
+    [Fact]
+    public void  Printinfor() {
+        
+    }}
+    
+
+ 
